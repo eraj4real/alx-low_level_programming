76b44f11ff7ef4a9224a57aab3835b9239888f8f
Jeff Erhabor <jeff.erhabor@gmail.com>
2023-02-16 20:53:13 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * A simple program that prints a line without the use of printf
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

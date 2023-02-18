ec9bbff49d749e223b57a4ba351f97920b333970
Jeff Erhabor <jeff.erhabor@gmail.com>
2023-02-17 20:05:33 -0800

    Task 1: Classifies a pseudo random number into + - 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main -A program that generates a pseudo random number and 
+ * classifies it as positive, zero or negative
+ * Return: 1 as any integer can be used,. Not only 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	} else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	} else
+		printf("%d is negative\n", n);
+	return (1);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+The codes here are for programs relating to if statements, else statements, and loops

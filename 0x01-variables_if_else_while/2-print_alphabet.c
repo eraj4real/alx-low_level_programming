4c1d31ba57126490da429d0984fed53a27f9b25f
Jeff Erhabor <jeff.erhabor@gmail.com>
2023-02-18 19:29:51 -0800

    Task 2 Print out alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Contains code that prints alphabets in lower case
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char harf = 'a';
+
+	for (harf = 'a'; harf <= 'z'; harf++)
+	{
+	putchar(harf);
+	}
+	putchar('\n');
+	return (0);
+}

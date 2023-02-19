4d44f7e085d02a797f0f41307b6e02ecf3277de9
Jeff Erhabor <jeff.erhabor@gmail.com>
2023-02-18 19:49:03 -0800

    Prints alphabets but excludes q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets in lower case
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char harf;
+
+	for (harf = 'a'; harf <= 'z'; harf++)
+	{
+		if (harf != 'q' && harf != 'e')
+		{
+			putchar(harf);
+		}
+	}
+	putchar('\n');
+	return (0);
+	
+}

3241540cfc1905be501233c4da5135a1a48b672d
Jeff Erhabor <jeff.erhabor@gmail.com>
2023-02-24 20:32:58 -0800

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,12 +8,13 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 48; i < 58; i++)
 	{
 	if (i != 4 || i != 2)
 	{
 		_putchar(i + '0');
 	}
+	continue;
 	}
 	_putchar('\n');
 }

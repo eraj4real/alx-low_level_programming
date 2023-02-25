5ca4ef7bbc1f7afbe355f468b07cf08245c17cc5
Jeff Erhabor <jeff.erhabor@gmail.com>
2023-02-24 18:42:20 -0800

    simple function to print numbers 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints a list of numbers 0 to 9 using a for loop
+ */
+
+void print_numbers(void)
+{
+
+	int i;
+
+	for (i = 0, i < 10, i++)
+		_putchar(i);
+
+	_putchar('\n');
+
+}

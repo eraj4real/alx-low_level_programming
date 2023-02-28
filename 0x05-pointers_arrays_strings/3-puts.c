3d07ad747bdaeb642a8f87ee2c87606dbd1cab59
Jeff Erhabor <jeff.erhabor@gmail.com>
2023-02-27 20:27:10 -0800

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * * _puts - function that prints a string, followed by a new line
+ * which is printed to the standard output except the nulcharacter in strings
+ * * @str: string pointer
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	/* While characterin string is not the null character */
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		 i++;
+	}
+
+	_putchar(LF);
+}

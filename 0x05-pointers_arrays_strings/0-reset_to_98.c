efc0ce2a8e067bf7438fc857cf3f7393110a8915
Jeff Erhabor <jeff.erhabor@gmail.com>
2023-02-27 19:08:46 -0800

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value  points to 98
+ * @n: int supplied as parameted to be referenced
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

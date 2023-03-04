1361572cf7842c2848c815f1054cd1675ae5187e
Jeff Erhabor <jeff.erhabor@gmail.com>
2023-03-03 20:43:55 -0800

    task 102 / 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer.
+ * @n: The integer supplied as parameter.
+ */
+
+void print_number(int n)
+{
+	int number = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		nummber = -number;
+	}
+
+	if ((number / 10) > 0)
+		print_number(number / 10);
+
+	_putchar((number % 10) + '0');
+}

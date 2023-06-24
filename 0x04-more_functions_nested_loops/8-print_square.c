510b3701a0bc9b926dc40ce78c0776fc86a7aa20
Jeff Erhabor <jeff.erhabor@gmail.com>
2023-06-23 19:16:18 -0700

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+
+#include "main.h"
+
+/**
+ * print_diagonal - prints a list of numbers 0 to 9 using a for loop
+ * @n: An integer that determines the number of lines that will be printed
+ */
+
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (j = 0; j < size; j++)
+		{
+			for (i = 1; i <= j; i++)
+			{
+				_putchar('#');
+			}
+		}
+	
+		_putchar('\n');
+	}
+}

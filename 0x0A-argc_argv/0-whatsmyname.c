20b845ad70b2c541e92833d35fdb0f769852bb79
Jeff Erhabor <jeff.erhabor@gmail.com>
2023-07-12 22:38:08 -0700

    Task  0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,12 +3,13 @@
 /**
  * main - main function
  * @argc: The character to print
- * @argv
+ * @argv: Array
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int main(int argc _attribute_((unused)), char *argv[])
+
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 	return (0);
